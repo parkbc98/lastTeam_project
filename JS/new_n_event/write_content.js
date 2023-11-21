c7ee168c7152112355acf0c2a66556d169aac00b
User -2023OPULM\Administrator <qwertui1217@gmail.com>
2023-11-21 19:54:06 +0900

    회원가입 페이지 진행 시작 => css완료 필요

---
+++ JS/new_n_event/write_content.js
@@ -0,0 +1,26 @@
+'use strict'
+
+let push_button = document.getElementById('push_button');
+
+let category = document.getElementById('category_box');
+let title = document.getElementById('title_section');
+let main_text = document.getElementById('main_text');
+
+
+// 항목 {
+//     1. news or event        완
+//     2. 제목                  완
+//     3. 작성자                
+//     4. 등록 시간
+//     5. 본문 내용
+//     6. url
+//     7. 이미지 첨부
+//     8. 비밀번호
+// }
+
+function create_content() {
+
+    
+}
+
+push_button.addEventListener('click', create_content());
