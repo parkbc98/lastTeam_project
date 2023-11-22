19f01f4abfb0c7116304958c96f68cfcbd05cf66
SH\BACK <bsh6954@gmail.com>
2023-11-22 18:08:30 +0900

    2023 11 22

---
+++ JS/QnA/QnA_main.js
@@ -0,0 +1,6 @@
+'user strict'
+
+let body = document.getElementsByTagName('body'),
+
+    main_pagelist = body.getElementsByClassName('qna_main_pagelist'),
+    qna_main_content = body.getElementsByClassName('qna_main_content');
---
+++ JS/QnA/QnA_prodchoice.js
@@ -0,0 +1,6 @@
+'user strict'
+
+let body = document.getElementsByTagName('body'),
+    prod_info = body.getElementsByClasseNave('prod_info'),
+    search_result_show = body.getElementsByClasseNave('search_result_show'),
+    shownum_prod = body.getElementsByClasseNave('shownum_prod');
