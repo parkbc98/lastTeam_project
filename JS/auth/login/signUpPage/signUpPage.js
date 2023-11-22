782d30a6afff66ebf13fbeff6a1c508c0c731e8b
User -2023OPULM\Administrator <qwertui1217@gmail.com>
2023-11-22 20:21:27 +0900

    야발

---
+++ JS/auth/login/signUpPage/signUpPage.js
@@ -0,0 +1,27 @@
+'use strict';
+
+console.log('안녕씨발아');
+
+let client_type = document.getElementsByClassName('client_type'),
+    reValues = document.getElementsByClassName('value'),
+    selValues = document.getElementsByClassName('selValue'),
+    submitBtn = document.getElementById('submit_button');
+
+
+
+// ===================================================================
+
+// 1. 빈칸 확인 value 클래스 들 빈칸 확인
+// 2. client type 확인 체크
+// 3. 아이디 체크 (영문대문자, 숫자 자릿수 체크)
+// 4. 비밀번호 (조합 확인)
+// 5. 비밀번호 확인과 같은지 확인
+// 6. 이름
+// 7. address 상세주소 필요하게 구현
+// 8. 전화번호
+// 9. 이메일 형식 확인
+
+// ===================================================================
+
+// 1. 빈칸 확인 value 클래스 들 빈칸 확인
+
