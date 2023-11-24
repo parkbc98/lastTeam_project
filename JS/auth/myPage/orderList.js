a0c2e0b70ac907f199abdc4cc5a85705dfec09fd
yewon <dforget9720@gmail.com>
2023-11-24 13:07:37 +0900

    주문내역리스트_자바스크립트_수정1

--- JS/auth/myPage/orderList.js
+++ JS/auth/myPage/orderList.js
@@ -1,25 +1,39 @@
-let normal = document.querySelector(".btn_normal");
 let dataselect = document.querySelector(".btn_dataSelect");
-console.log(normal)
 
-normal.addEventListener('click', (b) => {
-    b.target.style.backgroundColor = "black";
-    b.target.style.color = "white";
+let prevIndex = 0;
+let btnChanges = document.querySelector('.btn_selectList');
+let btnChangesLi = btnChanges.querySelectorAll('li');
+// btbChangese 안에 있는 li를 배열로 저장. 
 
-    let pro_box = document.querySelector('.pro_box');
 
-    if(pro_box.classList.contains('active')) {
-        pro_box.classList.remove('active');
-    } else {
-        pro_box.classList.add('active');
-    }
+let normal = document.querySelectorAll(".btn_normal").forEach((v, i) => {
+    v.addEventListener("click", (b) => {
+        let btnChangesLiA = btnChangesLi[prevIndex].querySelector('a');
+        btnChangesLiA.style.backgroundColor = "white";
+        btnChangesLiA.style.color = "black";
+
+        prevIndex = i;
+
+        let pro_box = document.querySelector('.pro_box');
+
+        b.target.style.backgroundColor = "black";
+        b.target.style.color = "white";
+
+        
+        // btnChanges.li[2]
+        if(pro_box.classList.contains('active')) {
+            pro_box.classList.remove('active');
+            
+        } else {
+            pro_box.classList.add('active');
+        }
+
+        
     });
+});
 
-dataselect.addEventListener('click', (b) => {
-    b.target.style.backgroundColor = "black";
-    b.target.style.color = "white";
-
-    let date = document.querySelector('.date_list');
+dataselect.addEventListener("click", () => {
+    let date = document.querySelector(".date_list");
 
     if(date.classList.contains('visibility')) {
         date.classList.remove('visibility');
