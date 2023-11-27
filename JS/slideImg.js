487ca6ebe4613c758af0d353481ecba2994c6285
User -2023ZSGDR\Administrator <parkbc98@naver.com>
2023-11-27 18:09:39 +0900

    ? 뭔가 했음 기억안남

--- JS/slideImg.js
+++ JS/slideImg.js
@@ -1,21 +1,29 @@
 export function slideImg (slideContainer, slideItems) {
     // 현재 idx, 동작시간, 
     // let currentIdx = 0;
+    let slideContainerModul = document.querySelector('.slide-container');
     let speedTime = 1000;
-    let movementNum = 21; // 21vm씩 이동
-    let calc = 1;
-
+    let movementNum = 20.25; // 21vm씩 이동
+    let calc = 0;
+    let slideLenght = slideItems.length;
     // 슬라이드요소를 끝단에 복제하기
-    let slideItemsClone = [...slideItems];
-    console.log(slideItemsClone);
-    slideItemsClone.forEach(element => {
-        
-    });
-    slideContainer.appendChiled(slideItemsClone);
+    let cloned = slideContainer.innerHtml;
+    // let slideItemsClone = [...slideItems];
+    // console.log(slideContainer);
+    // slideItemsClone.forEach((element, idx) => {
+    //     slideContainer.appendChiled(element);
+    // });
 
     setInterval(()=>{
-        if(slideItems.lenght == calc){
-            calc = 1;
+        if(calc == slideLenght){
+            slideContainer.classList.add('goFirst');
+            // console.log(slideContainer.classList);
+            calc = 0;
+            // console.log(slideContainer.style.left);
+            slideContainer.style.left = `-0vw`;
+            // console.log(slideContainer.style.left);
+            slideContainer.classList.remove('goFirst');
+            // console.log(slideContainer.classList);
         } else{
             calc++;
         }
