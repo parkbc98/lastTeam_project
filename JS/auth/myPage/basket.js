b1e6bc5aa4331a25fdd16a3ce49389da350c95b4
DESKTOP-5SQNM5E\deepr <lsw940617@gmail.com>
2023-11-28 14:53:04 +0900

    2023.11.28 <JS> 버튼 작동, 체크박스에 따른 총합계 변동,  a태그 HTML

--- JS/auth/myPage/basket.js
+++ JS/auth/myPage/basket.js
@@ -1,7 +1,4 @@
 
-// quota 팝업창 생성 (HTML CSS 필요)
-// 상품 가격 계산하기 
-
 
 //=== 객체 ===========================================================
 var MdContainer_list = [
@@ -11,6 +8,7 @@ var MdContainer_list = [
     package : '배송: [무료]/기본배송',
     number : 1,
     price : 130000,
+    sumPrice : 130000,
     option : '[옵션:35cm(=10,000)]'
 }
 , {
@@ -19,6 +17,7 @@ var MdContainer_list = [
     package : '배송: [무료]/기본배송',
     number : 1,
     price : 340000,
+    sumPrice : 340000,
     option : '[옵션:블랙]'
 }, {
     img : '../../../CSS/auth/myPage/limsw/ab87ec3ac74590998fe38992d08a7923.png"',
@@ -26,6 +25,7 @@ var MdContainer_list = [
     package : '배송: [무료]/기본배송',
     number : 1 ,
     price : 213000,
+    sumPrice : 213000,
     option : '[옵션:블랙]'
 }, {
     img : '../../../CSS/auth/myPage/limsw/edbacbcd8589cbdd53ebdefb11af12f3.jpg' ,
@@ -33,6 +33,7 @@ var MdContainer_list = [
     package : '배송: [무료]/기본배송',
     number : 1,
     price : 130000,
+    sumPrice : 130000,
     option : '[옵션:35cm(=10,000)]'
 }
 ];
@@ -47,7 +48,7 @@ for( i = 0; i < MdContainer_list.length ; i++) {
     makingFrame[0].innerHTML +=
     `<section class="MdContainer">
         <label class="mdList">
-        <input class="ckeckB" type="checkbox" name="checkedB" value="box" checked>
+        <input class="ckeckB" type="checkbox" name="checkedB" value="box">
         </label>
     
         <div class="mdList"><a href=""><img src="${MdContainer_list[i].img}" alt="상품 이미지"></a></div>
@@ -89,12 +90,12 @@ for( i = 0; i < MdContainer_list.length ; i++) {
         </div>
     
         <div class="mdList wishcontainer">
-            <input class="wishBox" type="button" value="삭제">
-            <input class="wishBox" type="button" value="WISH LIST">
+            <input class="wishBox deleteBox" type="button" value="삭제">
+            <input class="wishBox wishListBox" type="button" value="WISH LIST">
         </div>
     </section>`
 }
-    
+    console.log('ckeckB');
 
     
 // ====견적서 변수선언 ======================================================
@@ -110,6 +111,39 @@ quotaS.addEventListener('click',
     window.open('../../../HTML/auth/login/Login.html','팝업견적서','width=600px, height=700px');
 });
 
+// =====wish List======================================================
+let wishcontainer = document.getElementsByClassName('wishcontainer');
+let wishListBox = document.getElementsByClassName('wishListBox');
+let deleteBox = document.getElementsByClassName('deleteBox');
+
+// wishcontainer에 담긴 2개의 wishBox를 눌렀을 때, 
+// wishBox의 인덱스가 0인지 1인지 확인 후, 1 일때는 confirm이 나오도록 해야함. 
+// 클릭 이벤트가 발생 했을 때, if로 그 친구를 학인 해야함. 
+
+for(let i=0; i< wishcontainer.length;i++){
+    wishListBox[i].addEventListener('click',
+    ()=> {
+        console.log(`이렇게하면 될까?`);
+        alert(`관심상품 목록에 추가되었습니다. `)
+    });
+
+    deleteBox[i].addEventListener('click',
+    ()=> {
+        if(confirm(`선택하신 상품을 삭제하시겠습니까?`)){
+            yesDelete(); 
+        }else { 
+            NoDelete();
+        }
+    });
+}
+
+function yesDelete(){
+    alert('작동하지 않습니다.');
+}
+function NoDelete(){
+    alert('삭제 취소하셨습니다. ');
+    console.log('딜리트 하지 않음');
+}
     
 // ===== 옵션 박스 변수 =================================================
 
@@ -131,80 +165,84 @@ for (let i=0; i<optionWarp.length;i++){
         optionBox.classList.toggle('active');
     }
     // console.log(optionBox);
-
+    
     opChange.addEventListener('click',openclose);
-
+    
     md_btn_cancel[i].addEventListener('click',openclose);
     md_btn_change[i].addEventListener('click',openclose);
-
+    
 }
 
 // ====== 숫자 + - 하기 ===================================================
-// numF 의 value에 접근하기. 
+
 let numbering=document.getElementsByClassName('numF');
-let icon_plus=document.getElementsByClassName('icon_plus');
-let icon_minus=document.getElementsByClassName('icon_minus');
-let pricetag = document.querySelectorAll('.pricetag');
-let tot_price = document.querySelector('.lsContent>div>p:first-child');
+let icon_plus_minus=document.getElementsByClassName('material-symbols-outlined');
+let pricetag = document.getElementsByClassName('pricetag');
+let tot_price = document.querySelector('.lsContent>div>p');
+let pricetag_Amount = 0;
+// let priceFlag = 1;
 
-console.log(icon_plus)
+//===========================
 
-for(let i=0; i < numbering.length; i++){
-    
-    icon_plus[i].addEventListener('click',()=>{
-        let numbering_Plus = +(numbering[i].value) ;
-        numbering[i].value = ++(numbering_Plus);
-    
-        // 가격 계산하기
-        MdContainer_list[i].number = numbering[i].value;
-        let MdContainer_price = MdContainer_list[i].number * MdContainer_list[i].price;
+
+for(let i=0, icon_idx = 0; i < numbering.length; i++){
+    for (let j = 0; j < 2; j++) {
+        icon_plus_minus[icon_idx++].addEventListener('click',()=>{
+            const direct = j ? -1 : 1;
+            numbering[i].value = +numbering[i].value + direct;
+
+            // 수량이 변경될 때 sumPrice 갱신
+            MdContainer_list[i].sumPrice += MdContainer_list[i].price * direct;
+            console.log(MdContainer_list[i].sumPrice ) ;
             
-        // 가격 출력하기                
-        pricetag[i].innerText = `${MdContainer_price}`
+            pricetag[i].innerText = `${MdContainer_list[i].sumPrice}`;
 
-// ========= 곱한 MdContainer_price값을 더하면 배열 값을 더할 거라고 생각했으나 실패.  
-        // { 
-        // let totalAmount = 0;
-        // totalAmount += MdContainer_price;
-
-        // tot_price.innerText = `${totalAmount}`
-        // console.log(totalAmount)
-        // }
-
-// ========= 금액을 넣어보자=======================================================================================================
-
-        // tot_price.innerText = `${MdContainer_list[0].price}`
-        // console.log(`${MdContainer_list[0].price}원`);
-    })
+            // 체크박스가 체크된 경우 총 가격 갱신
+            if (checkB[i].checked) {
+                updateTotalPrice();
+            }
+        })
+    }
+    
 }
 
-// 가격 클래스 선언하기 
-console.log(pricetag);
 
-for (let i=0;i<numbering.length;i++){
-    icon_minus[i].addEventListener('click',()=>{
-        let numbering_minus = +(numbering[i].value);
+// ========= 체크 박스에서 체크확인 여부 ================================================================================================
 
+let checkB=document.getElementsByClassName('ckeckB');
+console.log(checkB);
 
-        if(numbering_minus > 0){
+// =============================================
+// checked에 이벤트를 걸어야함. => addevent click 칸에 
+// checkbox에 checked 되어 있는지 안되어 있는지 확인 => if () {}else{}
+// 체크가 되어 있으면 sumPrice 더하고
+// 체크가 되어 있지 않으면 priceSum을 더하지 않음
+// 그리고 그걸 pricetag_Amount 에 넣어 innerText 하기.
 
-            numbering[i].value = (numbering_minus-1);
-            
-            // 마이너스 가격 계산 및 출력하기
-            MdContainer_list[i].number = numbering[i].value;
-            let MdContainer_price = MdContainer_list[i].number * MdContainer_list[i].price;
+for (let i = 0; i < checkB.length; i++) {
 
-            pricetag[i].innerText = `${MdContainer_price}`
-            
-            
-        }
+    checkB[i].addEventListener('change',()=>{
         
-        //
+         if (checkB[i].checked) {
+            pricetag_Amount += MdContainer_list[i].sumPrice;
+        } else {
+            pricetag_Amount -= MdContainer_list[i].sumPrice;
+        }
 
+        tot_price.innerText = `${pricetag_Amount}` ;
     })
+                
 }
+//============체크박스 상태 변화에 따라 총 가격 업데이트==================================================================================
 
-// 가격을 전부 가져오기. 
+function updateTotalPrice() {
 
+    pricetag_Amount = 0;
 
-
+    for (let i = 0; i < checkB.length; i++) {
+        if (checkB[i].checked) {
+            pricetag_Amount += MdContainer_list[i].sumPrice;
+        }
+    }
+    tot_price.innerText = `${pricetag_Amount}`;
+}
